e2216dd31f139e67c52588a30187ac0e4ff7684f
dev-magicirlce <shaloti@ewhain.net>
2020-07-06 09:25:14 +0900

    첫번째이미지위치조정

--- js/value.js
+++ js/value.js
@@ -3,46 +3,48 @@ let result;
 function Eadd(){
     Evalue=sessionStorage.getItem('E');
     Evalue++;
-    console.log(Evalue);
+    console.log("E"+Evalue);
     sessionStorage.setItem('E', Evalue);
 }
 function Iadd(){
     Ivalue=sessionStorage.getItem('I');
     Ivalue++;
-    console.log(Ivalue);
+    console.log("I"+Ivalue);
     sessionStorage.setItem('I', Ivalue);
 }
 function Sadd(){
     Svalue=sessionStorage.getItem('S');
     Svalue++;
-    console.log(Svalue);
+    console.log("S"+Svalue);
     sessionStorage.setItem('S', Svalue);
 }
 function Nadd(){
     Nvalue=sessionStorage.getItem('N');
     Nvalue++;
-    console.log(Nvalue);
+    console.log("N"+Nvalue);
     sessionStorage.setItem('N', Nvalue);
 }
 function Tadd(){
     Tvalue=sessionStorage.getItem('T');
     Tvalue++;
-    console.log(Tvalue);
+    console.log("T"+Tvalue);
     sessionStorage.setItem('T', Tvalue);
 }
 function Fadd(){
     Fvalue=sessionStorage.getItem('F');
     Fvalue++;
-    console.log(Fvalue);
+    console.log("F"+Fvalue);
     sessionStorage.setItem('F', Fvalue);
 }
 function Jadd(){
     Jvalue=sessionStorage.getItem('J');
     Jvalue++;
+    console.log("J"+Fvalue);
     sessionStorage.setItem('J', Jvalue);
 }
 function Padd(){
     Pvalue=sessionStorage.getItem('P');
     Pvalue++;
+    console.log("P"+Fvalue);
     sessionStorage.setItem('P', Pvalue);
 }
